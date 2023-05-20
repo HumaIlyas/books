f21382305668f7c415f7e6df6353ea75399752be
HumaIlyas <hi.wtmconsult@gmail.com>
2023-05-20 21:14:23 +0000

    save files

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 let field = document.querySelector('textarea');
-let backUp = field.getAttribute('placeholder')
+let backUp = field.getAttribute('placeholder');
 let btn = document.querySelector('.btn');
-let reset = document.getElementById('reset')
-let submit = document.querySelector('#submit')
+let submit = document.querySelector('#submit');
 
 // const comments = document.querySelector('#comment-box')
 let comments = document.getElementById('comment-box');
 
 // array to store the comments
 let comments_arr = [];
-let comment_number= 0;
+let comment_count= 0;
 
 // to generate html list based on comments array
 let display_comments = () => {
@@ -21,15 +20,6 @@ let display_comments = () => {
   comments.innerHTML = list;
 }
 
-reset.onclick = function(event){
-  event.preventDefault();
-
-  // reset the array  
-   comments_arr.length = 0;
-  // re-genrate the comment html list
-  display_comments();
-}
-
 submit.onclick = function(event){
     event.preventDefault();
     let content = field.value;
@@ -37,11 +27,11 @@ submit.onclick = function(event){
       // if there is content
       // add the comment to the array
       comments_arr.push(content);
-      document.querySelector(".popup-message").innerHTML = 'Your comment has been submitted';
+      document.querySelector(".popup-message").innerHTML = 'Your comment has been submitted and displayed';
       // re-genrate the comment html list
       display_comments();
-      comment_number++;
-      document.getElementById("comment_number").innerHTML = comment_number;
+      comment_count++;
+      document.getElementById("comment_count").innerHTML = comment_count;
       // reset the textArea content 
       resetTextArea();
     }
@@ -52,5 +42,5 @@ let resetTextArea = () => {
     setTimeout(() => {
         field.value = '';
         document.querySelector(".popup-message").innerHTML = "";
-    }, 2000); 
+    }, 2500); 
 }
